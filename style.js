8e50ec2fde2f3db44492d0c1681128b59203d27d
Kauan Mateus <bueno.santos.kauan@escola.pr.gov.br>
2026-04-23 08:00:33 -0300

    aula

---
+++ style.js
@@ -0,0 +1,11 @@
+//Conversões Tipos de dados
+//Number = int ou float
+//String = texto ou char
+//Boolean = lógico
+
+//Conversões Implícita
+const numero = 456;
+const numString = '456';
+console.log(numero == numString); 
+
+//Conversões Explícitas
