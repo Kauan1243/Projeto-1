0279e512753c6d7fd3e4c16aff1a31ad1550bd4d
Kauan Mateus <bueno.santos.kauan@escola.pr.gov.br>
2026-04-23 08:12:35 -0300

    numeração

--- style.js
+++ style.js
@@ -6,6 +6,10 @@
 //Conversões Implícita
 const numero = 456;
 const numString = '456';
-console.log(numero == numString); 
+console.log(numero == numString); //Implícita
 
 //Conversões Explícitas
+console.log(numero + numString);
+
+console.log(numero + Number(numString)); //Explícitas
+console.log(String(numero) + numString); //Explícitas
