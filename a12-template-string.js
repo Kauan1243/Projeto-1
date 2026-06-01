57aff870ab2bcf16232810bbab61ca03de53c7aa
Kauan Mateus <bueno.santos.kauan@escola.pr.gov.br>
2026-06-01 09:07:07 -0300

    j

---
+++ a12-template-string.js
@@ -0,0 +1,9 @@
+const nome = Kauan
+const idade = 2026-2010
+const cidnascimento = "Dois Vizinhos"
+
+const apresentacao = "Meu nome é "+nome+ " minha idade é "+idade+" Minha cidade de nascimento é "+cidnascimento;
+console.log(apresentacao)
+
+const apresentacao2 = `Meu nomw é ${nome}, Minha idade é ${idade}, Minha cidade de nascimento é ${cidnascimento}`
+console.log(apresentacao2)
