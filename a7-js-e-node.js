e780153c7b673324aa7854fbd4c81636814bb096
Kauan Mateus <bueno.santos.kauan@escola.pr.gov.br>
2026-06-03 09:11:59 -0300

    novo conteudo 7

--- a7-js-e-node.js
+++ a7-js-e-node.js
@@ -1,5 +1,24 @@
-//Tipagem Dinâmica
-//Não precisa definir o tipo de dado que cada variável ou constante terá.
+// Tipagem dinamica: é você não precisar explicar qual tipo que a variavel é, Exemplo const int numero = 26 . Isso não é tipagem dinamica, 
+// tipagem dunamica é , Exemplo: const numero = 26 isso e tipagem dinamica em nenhum momento eu declarei se isso e um número ou um texto.
+// não precisa definir o tipo de dado que cada variavel ou constante terá.
 
-var num = '5';
+var num = "5";
+var ligado = 1;
+ligado = "569";
 
+// JavaScript
+// Linguagem Multiparadigma - resolvendo o mesmo problema utilizando diferentes abordagens.
+
+//ES6 /ES332015 - versão do JS lançada em 2015 com atualizações constantes
+
+//linguagem interpretada - vai analizando linha a linha diferente da linguagem compilada que gera 1 arquivo de maquina para executar. É mais rapida.
+//NodeJS - não é uma linguagem (é o js)
+//Interpretador de JS;
+//o JS, por ser uma linguagem interpretada, precisa de um interpretador
+
+//podemos utilar o navegador como interpretador
+
+//NodeJS é uma ferramenta que foi desenvolvida em 2010, bem depois que o javaScript foi lançado.
+
+//NodeJS norlmalmente é utilizado no back-and.
+//Deno é alternativo ao NodeJS.
