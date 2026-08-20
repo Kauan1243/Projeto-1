29a0d6baf027a5e7b1426d47826fb9c8c1b9e1ce
Kauan Mateus <bueno.santos.kauan@escola.pr.gov.br>
2026-08-20 11:46:47 -0300

    no wifi

---
+++ .vscode/a16-arrow-function.js
@@ -0,0 +1,40 @@
+// Arrow functions - flecha ( => )
+// 1º forma - declaração
+// 2º forma - expressão da função
+// 3º forma - arrow
+
+// arrow veio junto ES6
+
+const apresentarArrow = nome => `Meu nome é ${nome}`;
+console.log(apresentarArrow('Kauan'));
+
+const Apresentasoma = calculo => `o resultado da soma é: ${calculo}`;
+console.log(Apresentasoma(4 + 5));
+
+const verifica = (Num1, Num2) => Num1 && Num2 > 10;
+console.log(verifica(9, 8));
+
+const verificacao = num1 => {
+    if (num1 > 10){
+        return 'O Valor é maior que 10'
+    }else{
+        return 'O Valor é menor ou igual a 10'
+    }
+
+}
+
+console.log(verificacao(10));
+
+
+const calcular = (valor1, valor2) => {
+    if (valor1 < 10 & valor2 < 10){
+        return `O Resultado do calcúlo é:${valor1 * valor2} `
+    }else{
+        return `O Resultado do calcúlo é:${valor1 + valor2}`
+    }
+
+}
+
+console.log(calcular(12, 5))
+
+
